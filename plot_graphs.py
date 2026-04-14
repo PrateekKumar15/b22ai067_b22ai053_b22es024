9598b51f56b12cad8adb01ea8c47dc9de29ecb44
Prateek Kumar <prateekkumar72007@gmail.com>
2026-04-15 00:22:33 +0530

    chore: update QR code URL to point to the correct project repository

--- plot_graphs.py
+++ plot_graphs.py
@@ -38,7 +38,7 @@ def generate_qrcode():
     """Generates a QR code image to place into the poster"""
     print("Generating QR code...")
     qr = qrcode.QRCode(version=1, box_size=10, border=4)
-    qr.add_data("https://github.com/iit-jodhpur/noise-robust-wav2vec2")
+    qr.add_data("https://github.com/PrateekKumar15/Speech_Project.git")
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
     img.save('qrcode.png')
